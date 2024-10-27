9f3223c275755cf16c8533e81033c23c4ce37f11
pasco1284 <louey.saadaoui10@gmail.com>
2024-10-27 11:21:55 +0100

    siteweb

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['../composants/Header.astro']
+      }
+    }
+  }
+});
